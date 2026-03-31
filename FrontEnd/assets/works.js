a595c412c3c0e6c4525ec11275a6a38e2ddc646a
Florence Escanye <f.escanye@gmail.com>
2026-03-31 10:34:32 +0200

    changement boutons et filtres

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -1,6 +1,16 @@
-let works = []
+//La fonction init() sert à organiser le démarrage de l'application
+//dit dans quel ordre lancer les fonctions et attend les données avant d'agir
+//les données doivent être chargées avant de lancer les boutons
+async function init() {
+    await fetchWorks()
+    await fetchCategories()
+    btnCategories()
+}
+init()
+
 
 //création d'une fonction async pour récupérer les travaux de l'architecte depuis l'API
+let works = []
 async function fetchWorks() {
     //récupère et traite la réponse de l'API
     const response = await fetch("http://localhost:5678/api/works")
@@ -13,7 +23,7 @@ function loadWorks(works) {
     const gallery = document.querySelector(".gallery")
     gallery.innerHTML = "" 
     //création des éléments image dans la div gallery
-    for (i = 0; i < works.length; i++) {
+    for (let i = 0; i < works.length; i++) {
         //création d'une div pour l'image et le titre
         const figure = document.createElement("div")
 
@@ -36,53 +46,37 @@ function loadWorks(works) {
 //récupération initiale
 fetchWorks()
 
+
 //création boutons filtres
+let categories = []
+
+async function fetchCategories() {
+    const response = await fetch("http://localhost:5678/api/categories")
+    categories = await response.json()
+}
+
+function btnCategories() {
 const filters = document.querySelector(".filters")
 
-    //bouton tous
-    const filtreTous = document.createElement("button")
-    filtreTous.textContent = "Tous"
+// bouton "Tous"
+const btnTous = document.createElement("button")
+btnTous.textContent = "Tous"
+btnTous.addEventListener("click", () => {
+    loadWorks(works)
+})
+filters.appendChild(btnTous)
 
-    filtreTous.addEventListener("click", () => {
-        loadWorks(works)
-        console.log(works)
+//création des bouton pour chaque catégorie
+for (let i = 0; i < categories.length; i++) {
+    const category = categories[i]
+    const btnFiltre = document.createElement("button")
+    btnFiltre.textContent = category.name
+
+    btnFiltre.addEventListener("click", () => {
+        const filteredWorks = works.filter(works => works.categoryId === category.id)
+        loadWorks(filteredWorks)
     })
-    filters.appendChild(filtreTous)
+    filters.appendChild(btnFiltre)
+}
+}
 
-    //bouton objets
-    const filtreObjets = document.createElement("button")
-    filtreObjets.textContent = "Objets"
-
-    filtreObjets.addEventListener("click", () => {
-    const objets = works.filter(works => works.categoryId === 1)
-    
-    //effacement de l'écran et régénération de la page avec les images filtrées
-    document.querySelector(".gallery").innerHTML = ""
-    loadWorks(objets)
-    console.log(objets)
-    })
-    filters.appendChild(filtreObjets)
-
-    //bouton appartements
-    const filtreAppartements = document.createElement("button")
-    filtreAppartements.textContent = "Appartements"
-
-    filtreAppartements.addEventListener("click", () => {
-    const appartements = works.filter(works => works.categoryId === 2)
-    document.querySelector(".gallery").innerHTML = ""
-    loadWorks(appartements)
-    console.log(appartements)
-    })
-    filters.appendChild(filtreAppartements)
-
-    //bouton hôtels
-    const filtreHotels = document.createElement("button")
-    filtreHotels.textContent = "Hôtels & restaurants"
-    
-    filtreHotels.addEventListener("click", () => {
-    const hotels = works.filter(works => works.categoryId === 3)
-    document.querySelector(".gallery").innerHTML = ""
-    loadWorks(hotels)
-    console.log(hotels)
-    })
-    filters.appendChild(filtreHotels)
